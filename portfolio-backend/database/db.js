ecb7ab34d2679927ff82dd894ed956ef9b4b0f62
lenodev5 <lenodev5@gmail.com>
2026-01-12 17:01:11 +0300

    initail commit

--- portfolio-backend/database/db.js
+++ portfolio-backend/database/db.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 const mongodb_URL = process.env.mongodb_URL;
 
-mongoose.connect(mongodb_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(mongodb_URL)
+.then(() => {
     console.log('Connected to MongoDB');        
 }).catch((error) => {
     console.error('Error connecting to MongoDB', error.message);
