d398629aba5aca2e1532afc97d86c3773c3327d0
lenodev5 <lenodev5@gmail.com>
2026-02-03 14:21:12 +0300

    update dashboard css

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -12,7 +12,6 @@ import './style/about1.css'
 import './style/achivements.css'
 import './style/contact.css'
 import './style/service.css'
-import "./style/dashboard.css"
 import './style/login.css'
 import './style/dashboard.css'
 
